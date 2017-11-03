91ea6ecbaca05c8bec1f811dc2ec2f0b3ebfafca
Damien Corticchiato <damien.corticchiato@2spark.com>
2017-11-03 14:37:21 +0100

    first steps into animated sprites

--- main.lua
+++ main.lua
@@ -5,6 +5,10 @@ local position = {
 local direction = 'stand'
 local canShoot = true
 local shoots = {}
+local spriteState = {
+  y = 1,
+  x = 2
+}
 
 
 function love.keypressed(key)
@@ -61,30 +65,58 @@ function updateShoots()
 end
 
 function move(direction)
-  local step = 5;
+  local step = 2;
   if love.keyboard.isDown("up") then
+    spriteState.y = 4
     position.y = position.y - step;
   elseif love.keyboard.isDown("down") then
+    spriteState.y = 1
     position.y = position.y + step;
   end
   if love.keyboard.isDown("left") then
+    spriteState.y = 2
     position.x = position.x - step;
   elseif love.keyboard.isDown("right") then
+    spriteState.y = 3
     position.x = position.x + step;
   end
 end
 
 function love.load()
-  isaac = love.graphics.newImage("sprite.png")
+  isaac = love.graphics.newImage("isaac_spritesheet.png")
 end
-
-function love.update()
+local animationTimer = 0
+function love.update(timing)
+  animationTimer = animationTimer + timing
+  print(animationTimer)
+  if(animationTimer > 0.2) then
+    spriteState.x = spriteState.x + 1
+    if(spriteState.x > 3) then
+      spriteState.x = 1
+    end
+    animationTimer = 0
+  end
   move()
   updateShoots()
 end
 
 function love.draw()
-  love.graphics.draw(isaac, position.x, position.y)
+  local width, height = isaac:getDimensions()
+  print(
+    width/3 * (spriteState.x - 1),
+    height/4 * (spriteState.y - 1)
+  )
+  local quad = love.graphics.newQuad(
+    width/3 * (spriteState.x - 1),
+    height/4 * (spriteState.y - 1),
+    width/3,
+    height/4,
+    isaac:getDimensions()
+  )
+
+  love.graphics.scale(1.5, 1.5)
+  love.graphics.draw(isaac, quad, position.x, position.y)
+
   local i = 1
   love.graphics.setColor(255, 255, 255)
   while shoots[i] do
